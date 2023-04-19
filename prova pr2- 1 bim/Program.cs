7edb224f6b66eb78f09d4c8051612122aebff130
CJ3016595 <CJ3016595@LAB01-PC31>
2023-04-19 12:52:07 -0300

    Adicionar arquivos de projeto.

---
+++ prova pr2- 1 bim/Program.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace prova_pr2__1_bim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //questao 1
+
+            //  int numero;
+            //  Console.WriteLine("digite um numero");
+            //numero=int.Parse(Console.ReadLine());
+
+            //  if (numero < 0)
+            //  {
+            //      numero = -numero;
+
+            //  }
+            //  Console.WriteLine("o valor absoluto é:" +numero);
+            //  Console.ReadKey();
+
+            //  questão 2
+
+            //int numero;
+            //Console.WriteLine("digite um numero");
+            //numero = int.Parse(Console.ReadLine());
+
+            //if (numero % 3 == 0 && numero % 5 == 0)
+            //{
+            //    Console.WriteLine("o numero é multiplo por 3 e 5");
+            //}
+            //else if (numero % 4 == 0 && numero % 7 == 0)
+            //{
+            //    Console.WriteLine("o numero é divisivel por 4 e 7" + numero);
+            //}
+            //else if (numero % 3 == 0)
+            //{
+
+            //    Console.WriteLine("o numero é divisivel por 3" + numero);
+            //}
+            //else if (numero % 5 == 0)
+            //{
+
+            //    Console.WriteLine("o numero é divisivel por 5" + numero);
+
+            //}
+            //else if (numero % 4 == 0)
+            //{
+
+            //    Console.WriteLine("o numero é divisivel por 4" + numero);
+
+            //}
+            //else if (numero % 7 == 0)
+            //{
+
+            //    Console.WriteLine("o numero é divisivel por 7" + numero);
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("nao é possivel dividir o mumero por nenhuma" + numero);
+            //    Console.ReadKey();
+            //}
+
+
+            // questão 3
+            //int num1, num2, num3;
+
+            //Console.WriteLine("Digite o primeiro número: ");
+            //num1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Digite o segundo número: ");
+            //num2 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Digite o terceiro número: ");
+            //num3 = int.Parse(Console.ReadLine());
+
+            //if (num1 >= num2 && num1 >= num3)
+            //    Console.WriteLine("O maior número é:", num1);
+
+            //else if (num2 >= num1 && num2 >= num3)
+            //{
+
+            //    Console.WriteLine("O maior número é:", num2);
+            //}
+
+            //else if (num3 >= num1 && num3 >= num2)
+            //{
+            //    Console.WriteLine("O maior número é:", num3);
+            //    Console.ReadKey();
+
+
+
+            //exercicio 4
+            ////Console.Write("Digite um número de 1 a 7: ");
+            //int diaSemana = int.Parse(Console.ReadLine());
+
+            //switch (diaSemana)
+            //{
+            //    case 1:
+            //        Console.WriteLine("Você assiste aula de Matemática aos domingos.");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("Você assiste aula de Português às segundas.");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("Você assiste aula de História às terças.");
+            //        break;
+            //    case 4:
+            //        Console.WriteLine("Você assiste aula de Geografia às quartas.");
+            //        break;
+            //    case 5:
+            //        Console.WriteLine("Você assiste aula de Física às quintas.");
+            //        break;
+            //    case 6:
+            //        Console.WriteLine("Você assiste aula de Química às sextas.");
+            //        break;
+            //    case 7:
+            //        Console.WriteLine("Você assiste aula de Biologia aos sábados.");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Número inválido, digite um número de 1 a 7.");
+            //        break;
+            //}
+
+            //// Exibe mensagem adicional para finais de semana
+            //if (diaSemana == 1 || diaSemana == 7)
+
+            // questao 5
+            //int numero, maior, menor;
+            //Console.Write("Digite um número inteiro: ");
+            //numero = int.Parse(Console.ReadLine());
+
+
+            //maior = numero;
+            //menor = numero;
+
+            //while (numero != 0)
+            //{
+
+            //    Console.Write("Digite outro número inteiro: ");
+            //    numero = int.Parse(Console.ReadLine());
+
+            //    if (numero > maior)
+            //    {
+            //        maior = numero;
+            //    }
+            //    else if (numero < menor && numero != 0)
+            //    {
+            //        menor = numero;
+             //             }
+            // Console.WriteLine("O maior número é: " + maior);
+             // Console.WriteLine("O menor número é: " + menor);
+
+
+
+            //exercicio 6
+           // não consegui fazer o resto, agradeço a compreensão. Vem recu :(
+           // i am sorry.
+            }
+            
+    } 
+}
